c98e88cda80d3a9d2994b6c4ecfa57622b45600c
jmanjot <jmanjot@users.noreply.github.com>
2015-07-04 16:42:16 +0200

    Add airport pairs feature

--- feature_extractor.py
+++ feature_extractor.py
@@ -18,6 +18,7 @@ class FeatureExtractor(object):
         left_on=['DateOfDeparture'], 
         right_on=['DateOfDeparture'], sort=False)
 
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['Departure']+data_encoded['Arrival'], prefix='v'))
         data_encoded = data_encoded.join(pd.get_dummies(data_encoded['Departure'], prefix='d'))
         data_encoded = data_encoded.join(pd.get_dummies(data_encoded['Arrival'], prefix='a'))
         data_encoded = data_encoded.drop('Departure', axis=1)
@@ -26,20 +27,20 @@ class FeatureExtractor(object):
         # following http://stackoverflow.com/questions/16453644/regression-with-date-variable-using-scikit-learn
         data_encoded['DateOfDeparture'] = pd.to_datetime(data_encoded['DateOfDeparture'])
         # data_encoded['year'] = data_encoded['DateOfDeparture'].dt.year
-        # data_encoded['month'] = data_encoded['DateOfDeparture'].dt.month
+        data_encoded['month'] = data_encoded['DateOfDeparture'].dt.month
         # data_encoded['day'] = data_encoded['DateOfDeparture'].dt.day
         data_encoded['weekday'] = data_encoded['DateOfDeparture'].dt.weekday
-        data_encoded['week'] = data_encoded['DateOfDeparture'].dt.week
+        # data_encoded['week'] = data_encoded['DateOfDeparture'].dt.week
         data_encoded['n_days'] = data_encoded['DateOfDeparture'].apply(lambda date: (date - pd.to_datetime("1970-01-01")).days)
 
         # data_encoded = data_encoded.join(pd.get_dummies(data_encoded['year'], prefix='y'))
-        # data_encoded = data_encoded.join(pd.get_dummies(data_encoded['month'], prefix='m'))
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['month'], prefix='m'))
         # data_encoded = data_encoded.join(pd.get_dummies(data_encoded['day'], prefix='d'))
         data_encoded = data_encoded.join(pd.get_dummies(data_encoded['weekday'], prefix='wd'))
-        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['week'], prefix='w'))
-        
+        # data_encoded = data_encoded.join(pd.get_dummies(data_encoded['week'], prefix='w'))
+
+        data_encoded = data_encoded.drop('month', axis=1)
         data_encoded = data_encoded.drop('weekday', axis=1)
-        data_encoded = data_encoded.drop('week', axis=1)
         data_encoded = data_encoded.drop('std_wtd', axis=1)
         data_encoded = data_encoded.drop('DateOfDeparture', axis=1)
         
